8abd5a0fabddf3eef5dcc3f0f0681e58cb56bfc7
hustaia <49987555+hustaia@users.noreply.github.com>
2021-01-24 18:16:24 +0800

    Add files via upload

---
+++ prepare_dataset/normalize_QNRF.py
@@ -0,0 +1,30 @@
+import os
+import glob
+import scipy.io as io
+from PIL import Image
+
+                                         
+path = "/data0/dataset/chengjian/UCF_QNRF/Train/"
+path_new = path.replace('UCF_QNRF','UCF_QNRF_normalized')
+if  not os.path.exists(path_new):
+    os.makedirs(path_new)
+for img_path in glob.glob(os.path.join(path, '*.jpg')): 
+        img = Image.open(img_path).convert('RGB')        
+        w, h = img.size
+        mat_path = img_path.replace('.jpg','_ann.mat')
+        mat = io.loadmat(mat_path)
+        gt = mat["annPoints"] 
+        if w > 1920 or h > 1920:
+            print(img_path)
+            factor = min(1920 / float(w), 1920 / float(h))
+            h_new = int(h * factor)
+            w_new = int(w * factor)
+            img_new = img.resize((w_new, h_new), Image.ANTIALIAS)
+            gt_new = gt * factor
+        else:
+            img_new = img
+            gt_new = gt
+        img_path_new = img_path.replace('UCF_QNRF','UCF_QNRF_normalized')
+        img_new.save(img_path_new, quality=100)    
+        mat_path_new = mat_path.replace('UCF_QNRF','UCF_QNRF_normalized')
+        io.savemat(mat_path_new, {'annPoints': gt_new}) 
