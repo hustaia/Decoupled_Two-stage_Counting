08815748b45f0f6e6961f00d04f83e457d960c04
hustaia <49987555+hustaia@users.noreply.github.com>
2021-01-25 17:34:21 +0800

    Delete generate_synthesized_dataset directory

--- generate_synthesized_dataset/draw_coordinate.m
+++
@@ -1,8 +0,0 @@
-function draw_coordinate(a, b, center_x, center_y)
-
-% a: long  axis( X )
-% b: short axis( Y )
-d = 0:360;
-x = a * cosd(d);
-y = b * sind(d);
-patch(x + center_x, y + center_y, 'b', 'edgecolor', 'none', 'facealpha',0.8);   %²»Í¸Ã÷¶È
